82b657868378b22888fad834f13498cd811dd8cf
Epixu <dimomarkov@gmail.com>
2024-06-03 20:10:14 +0300

    Cosmetic changes

--- test/TestWindow.cpp
+++ test/TestWindow.cpp
@@ -23,10 +23,7 @@ SCENARIO("Window creation", "[window]") {
    for (int repeat = 0; repeat != 10; ++repeat) {
       GIVEN(std::string("Init and shutdown cycle #") + std::to_string(repeat)) {
          // Create root entity                                          
-         Thing root;
-         root.SetName("ROOT");
-         root.CreateRuntime();
-         root.LoadMod("GLFW");
+         auto root = Thing::Root<false>("GLFW");
          
          WHEN("The window is created via abstraction") {
             auto window = root.CreateUnit<A::Window>();
